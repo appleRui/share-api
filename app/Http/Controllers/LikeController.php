7131834c2ca783e2105810d620b176831e862ae3
appleRui <apple31581@gmail.com>
2021-10-05 14:07:11 +0900

    feature/211005/nices

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Like;
+use Log;
+
+class LikeController extends Controller
+{
+    //
+    public function store(Request $request){
+        $like = new Like();
+        $like->user_id = $request->user_id;
+        $like->post_id = $request->post_id;
+        $like->save();
+        $likes_count = Post::likeCount($like->post_id);
+        $data = ['result' => true, 'like_count' => $likes_count];
+        return response()->json([
+            'data' => $data
+        ], 200);
+    }
+
+    public function destroy(Request $request){
+        $like = Like::where([['post_id', $request->post_id], ['user_id', $request->user_id]])->first();
+        $like->delete();
+        $likes_count = Post::likeCount($request->post_id);
+        $data = ['result' => false, 'like_count' => $likes_count];
+        return response()->json([
+            'data' => $data
+        ], 200);
+    }
+
+
+    public function hasLike(Request $request){
+        Log::info($request);
+        $like = Like::where([['post_id', $request->post_id], ['user_id', $request->user_id]])->exists();
+        if($like){
+            $res = true;
+        }else{
+            $res = false;
+        }
+        return response()->json([
+            'result' => $res
+        ], 200);
+    }
+}
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Log;
 
 class Post extends Model
 {
@@ -11,8 +12,23 @@ class Post extends Model
 
     protected $guarded = array('id');
 
+    public function likes()
+    {
+        return $this->hasMany('App\Models\Like');
+    }
+
     public static $rules = array(
         'post' => 'required',
         'user_id' => 'required'
     );
+
+    public static function likeCount($post_id){
+        // postのid渡してカウント取得
+        $count = self::find($post_id)->likes()->count();
+        return $count;
+    }
+
+    //public function isLiked($user_id){
+    //    return $this->likes()->where('user_id', $user_id)->exists();
+    //}
 }
